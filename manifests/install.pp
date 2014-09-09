159d8e0ab5a86240b878a4cb0566c1becf8f35e9
David Otton <david@otton.org>
2014-09-09 13:47:11 +0100

    Rely on beluga for JDK and wget

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,13 +2,6 @@
 # Pre-conditions that must be met before Fuseki can be installed
 
 class fuseki::install {
-  include fuseki::params
-
-  package { 'default-jdk':
-    ensure  => present,
-  }
-
-  package { 'wget':
-    ensure  => present,
-  }
+  include beluga::wget
+  include beluga::java
 }
