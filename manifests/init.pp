a19f6d6232e14d1d1cf4b9e93e83fe1b12c743c8
Noel Sharpe <noels@radnetwork.co.uk>
2015-03-25 11:26:26 +0000

    add fuseki_data_dir param to fuseki class.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,12 @@
 #
 class fuseki (
   $fuseki_version = $fuseki::params::fuseki_version
+  $fuseki_data_dir  = $fuseki::params::fuseki_data_dir,
 ) inherits fuseki::params {
   class {'fuseki::install': } ->
-  class {'fuseki::config': } ~>
+  class {'fuseki::config':
+    fuseki_data_dir  => $fuseki_data_dir,
+  } ~>
   class {'fuseki::service': } ->
   Class['fuseki']
 }
